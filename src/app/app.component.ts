f5b74421c4063df8f03510830a049e73df477687
Saúl Laínez <saullainez@hotmail.es>
2021-01-02 11:54:25 -0600

    Pipe: json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,14 @@ export class AppComponent {
   PI : number = Math.PI;
   porcentaje : number = 0.234;
   salario: number = 1234.5;
+
+  heroe = {
+    nombre : 'Logan',
+    clave : 'Wolverine',
+    edad : 500,
+    direccion : {
+      calle : 'Primera',
+      casa : 20
+    }
+  };
 }
