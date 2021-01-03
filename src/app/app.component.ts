541e0f2566fb43cd2fdc743021ffb3bc1ed36b9f
Saúl Laínez <saullainez@hotmail.es>
2021-01-03 16:49:36 -0600

    Pipe personalizado: capitalizar palabras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Saúl Laínez';
+  nombre2: string = 'saUl lAInez';
 
   arreglo = [1,2,3,4,5,6,7,8,9,10];
   PI : number = Math.PI;
